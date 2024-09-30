c613a2efffefd74dcbffc06f3c7902e1a4310a1e
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-09-30 14:46:11 +0700

    Add more bugs

--- website/server/main.py
+++ website/server/main.py
@@ -1,8 +1,11 @@
 """Application backend."""
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"status": "ready"}
+
+uvicorn.run(app, host="0.0.0.0", port=8888)

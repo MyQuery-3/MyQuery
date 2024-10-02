631992cd3514f25495270567ad9999f88acf997f
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-10-02 12:11:15 +0700

    feat: Add shadcnui lib, Add loading.tsx, Fix some style

--- website/client/app/page.tsx
+++ website/client/app/page.tsx
@@ -1,16 +1,21 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import "./style.css"
+import Link from "next/link";
+import "./style.css";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
       <div className="flex flex-col space-y-2 items-center justify-center content-center min-h-screen">
-        <p className="text-[#E35205] text-[5rem] text-center font-bold scale-up-center max-[470px]:text-[3rem] ">Query Mind</p>
-        <Link href={"/playground"} className="bg-[#2D569E] text-[1.5rem] text-white p-2 rounded-md self-center duration-300 ease-in-out before:opacity-0 after:opacity-100">Start Learning</Link>
+        <p className="text-[#E35205] text-[5rem] text-center font-bold scale-up-center max-[470px]:text-[3rem] ">
+          Query Mind
+        </p>
+        <Button asChild size={"lg"} variant={"default"}>
+          <Link href={"/playground"}>Start Learning</Link>
+        </Button>
       </div>
-      <div className=' absolute rounded-full w-5 h-5 bg-slate-600 top-[50%] left-[50%] scale-up-fade'></div>
+      <div className=" absolute rounded-full w-5 h-5 bg-slate-600 top-[50%] left-[50%] scale-up-fade"></div>
     </main>
   );
 }
--- website/client/app/playground/page.tsx
+++ website/client/app/playground/page.tsx
@@ -1,84 +1,90 @@
-export default function page() {
-    return (
-      <main className="">
-        <div className=" grid grid-flow-col">
-          <div>
-            <div className="m-5 mx-[100px] p-2 min-h-[300px] rounded-md bg-[#cbcbcb]">
-              <textarea className="w-full min-h-[300px] p-5 text-lg rounded-md" />
-            </div>
-            <div id="result" className=" bg-[#cbcbcb] rounded-md m-10">
-              <p className="text-2xl font-bold ml-3 mt-3">Database</p>
-              <div className="p-10">
-                <table className="min-w-full text-left text-sm whitespace-nowrap">
-                  <thead className="uppercase tracking-wider border-b-2 dark:border-neutral-600">
-                    <tr>
-                      <th scope="col" className="px-6 py-4">
-                        Product
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Price
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Stock
-                      </th>
-                      <th scope="col" className="px-6 py-4">
-                        Status
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="border-b dark:border-neutral-600">
-                      <th scope="row" className="px-6 py-4">
-                        Handbag
-                      </th>
-                      <td className="px-6 py-4">$129.99</td>
-                      <td className="px-6 py-4">30</td>
-                      <td className="px-6 py-4">In Stock</td>
-                    </tr>
-                    <tr className="border-b dark:border-neutral-600">
-                      <th scope="row" className="px-6 py-4">
-                        Shoes
-                      </th>
-                      <td className="px-6 py-4">$89.50</td>
-                      <td className="px-6 py-4">25</td>
-                      <td className="px-6 py-4">In Stock</td>
-                    </tr>
+export default function Page() {
+  return (
+    <div></div>
+  )
+}
+
+// export default function page() {
+//     return (
+//       <main className="">
+//         <div className=" grid grid-flow-col">
+//           <div>
+//             <div className="m-5 mx-[100px] p-2 min-h-[300px] rounded-md bg-[#cbcbcb]">
+//               <textarea className="w-full min-h-[300px] p-5 text-lg rounded-md" />
+//             </div>
+//             <div id="result" className=" bg-[#cbcbcb] rounded-md m-10">
+//               <p className="text-2xl font-bold ml-3 mt-3">Database</p>
+//               <div className="p-10">
+//                 <table className="min-w-full text-left text-sm whitespace-nowrap">
+//                   <thead className="uppercase tracking-wider border-b-2 dark:border-neutral-600">
+//                     <tr>
+//                       <th scope="col" className="px-6 py-4">
+//                         Product
+//                       </th>
+//                       <th scope="col" className="px-6 py-4">
+//                         Price
+//                       </th>
+//                       <th scope="col" className="px-6 py-4">
+//                         Stock
+//                       </th>
+//                       <th scope="col" className="px-6 py-4">
+//                         Status
+//                       </th>
+//                     </tr>
+//                   </thead>
+//                   <tbody>
+//                     <tr className="border-b dark:border-neutral-600">
+//                       <th scope="row" className="px-6 py-4">
+//                         Handbag
+//                       </th>
+//                       <td className="px-6 py-4">$129.99</td>
+//                       <td className="px-6 py-4">30</td>
+//                       <td className="px-6 py-4">In Stock</td>
+//                     </tr>
+//                     <tr className="border-b dark:border-neutral-600">
+//                       <th scope="row" className="px-6 py-4">
+//                         Shoes
+//                       </th>
+//                       <td className="px-6 py-4">$89.50</td>
+//                       <td className="px-6 py-4">25</td>
+//                       <td className="px-6 py-4">In Stock</td>
+//                     </tr>
   
-                    <tr className="border-b dark:border-neutral-600">
-                      <th scope="row" className="px-6 py-4">
-                        Bedding Set
-                      </th>
-                      <td className="px-6 py-4">$69.99</td>
-                      <td className="px-6 py-4">40</td>
-                      <td className="px-6 py-4">In Stock</td>
-                    </tr>
-                    <tr className="border-b dark:border-neutral-600">
-                      <th scope="row" className="px-6 py-4">
-                        Dining Table
-                      </th>
-                      <td className="px-6 py-4">$449.99</td>
-                      <td className="px-6 py-4">5</td>
-                      <td className="px-6 py-4">In Stock</td>
-                    </tr>
-                    <tr className="border-b dark:border-neutral-600">
-                      <th scope="row" className="px-6 py-4">
-                        Soap Set
-                      </th>
-                      <td className="px-6 py-4">$24.95</td>
-                      <td className="px-6 py-4">50</td>
-                      <td className="px-6 py-4">In Stock</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+//                     <tr className="border-b dark:border-neutral-600">
+//                       <th scope="row" className="px-6 py-4">
+//                         Bedding Set
+//                       </th>
+//                       <td className="px-6 py-4">$69.99</td>
+//                       <td className="px-6 py-4">40</td>
+//                       <td className="px-6 py-4">In Stock</td>
+//                     </tr>
+//                     <tr className="border-b dark:border-neutral-600">
+//                       <th scope="row" className="px-6 py-4">
+//                         Dining Table
+//                       </th>
+//                       <td className="px-6 py-4">$449.99</td>
+//                       <td className="px-6 py-4">5</td>
+//                       <td className="px-6 py-4">In Stock</td>
+//                     </tr>
+//                     <tr className="border-b dark:border-neutral-600">
+//                       <th scope="row" className="px-6 py-4">
+//                         Soap Set
+//                       </th>
+//                       <td className="px-6 py-4">$24.95</td>
+//                       <td className="px-6 py-4">50</td>
+//                       <td className="px-6 py-4">In Stock</td>
+//                     </tr>
+//                   </tbody>
+//                 </table>
+//               </div>
+//             </div>
+//           </div>
   
-          <div className="bg-[#cbcbcb] my-10 mr-10 rounded-md">
-            <p className=" text-center mt-3 text-2xl font-blod">SETTING</p>
-          </div>
-        </div>
-      </main>
-    );
-  }
+//           <div className="bg-[#cbcbcb] my-10 mr-10 rounded-md">
+//             <p className=" text-center mt-3 text-2xl font-blod">SETTING</p>
+//           </div>
+//         </div>
+//       </main>
+//     );
+//   }
   

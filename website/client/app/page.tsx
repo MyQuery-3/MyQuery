9156490794ec17c01259ac1555a485ef9e54d3b4
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-10-02 11:25:03 +0700

    create css

--- website/client/app/page.tsx
+++ website/client/app/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import Link from 'next/link'
+import "./style.css"
 
 export default function Home() {
   return (
     <main className="">
       <div className="flex flex-col space-y-2 items-center justify-center content-center min-h-screen">
-        <p className="text-[#E35205] text-[5rem] text-center font-bold scale-up-center">Query Mind</p>
-        <Link href={"/playground"} className="bg-[#2D569E] text-[1.5rem] text-white p-2 rounded-md self-center">Start Learning</Link>
+        <p className="text-[#E35205] text-[5rem] text-center font-bold scale-up-center max-[470px]:text-[3rem] ">Query Mind</p>
+        <Link href={"/playground"} className="bg-[#2D569E] text-[1.5rem] text-white p-2 rounded-md self-center duration-300 ease-in-out before:opacity-0 after:opacity-100">Start Learning</Link>
       </div>
+      <div className=' absolute rounded-full w-5 h-5 bg-slate-600 top-[50%] left-[50%] scale-up-fade'></div>
     </main>
   );
 }

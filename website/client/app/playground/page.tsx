3ab2e9a153ab5562a42464daaae3bb1eafc399c2
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-10-09 11:42:31 +0700

    Connect playground frontend to grader backend

--- website/client/app/playground/page.tsx
+++ website/client/app/playground/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import React from "react";
 import Editor from "@monaco-editor/react";
 import { useRef } from "react";
@@ -9,34 +10,40 @@ export default function page() {
   const handleEdotorDidMount = (editor: any, monaco: any) => {
     editorRef.current = editor;
   };
-  const handleExecute = () => {
-    const code = editorRef.current.getValue();
-    console.log(code);
+  const handleExecute = async () => {
+    try {
+      const code = editorRef.current.getValue();
+      await axios.post("http://localhost:8787/grader/execute", { query: code });
+    } catch (error) {
+      console.error("Error executing query:", error);
+    }
   };
   return (
     <main className="max-w-screen w-full min-h-screen bg-black px-10 py-2">
       <div className="container mx-auto flex gap-6">
-      <div className="w-3/4 space-y-6">
-        <div className="bg-white p-6 rounded-xl">
-        <Editor
-          height="40vh"
-          defaultLanguage="sql"
-          defaultValue="SELECT * FROM table"
-          options={{ fontSize: 18 }}
-          onMount={handleEdotorDidMount}
-        />
-        <div className="flex justify-end gap-x-6">
-          <Button onClick={handleExecute}>Execute</Button>
-          <Button variant="outline" onClick={handleExecute}>Reset</Button>
+        <div className="w-3/4 space-y-6">
+          <div className="bg-white p-6 rounded-xl">
+            <Editor
+              height="40vh"
+              defaultLanguage="sql"
+              defaultValue="SELECT * FROM table"
+              options={{ fontSize: 18 }}
+              onMount={handleEdotorDidMount}
+            />
+            <div className="flex justify-end gap-x-6">
+              <Button onClick={handleExecute}>Execute</Button>
+              <Button variant="outline" onClick={handleExecute}>
+                Reset
+              </Button>
+            </div>
+          </div>
+          <div className="bg-white p-6 rounded-xl h-[40vh]">
+            หน้าสำหรับแสดงผลลัพธ์ของการ Query
+          </div>
         </div>
+        <div className="bg-white p-6 rounded-xl w-1/4">
+          หน้าสำหรับการแสดงDbต่างๆที่เลือกได้
         </div>
-        <div className="bg-white p-6 rounded-xl h-[40vh]">
-        หน้าสำหรับแสดงผลลัพธ์ของการ Query
-        </div>
-      </div>
-      <div className="bg-white p-6 rounded-xl w-1/4">
-        หน้าสำหรับการแสดงDbต่างๆที่เลือกได้
-      </div>
       </div>
     </main>
   );

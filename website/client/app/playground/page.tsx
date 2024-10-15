7f7368df4fa0c09d6ed5cae29200cf6bdb2e325e
suwizx <suwijak.pak@gmail.com>
2024-10-15 12:55:01 +0700

    ไม่บอก

--- website/client/app/playground/page.tsx
+++ website/client/app/playground/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRef } from "react"
 import Editor ,{ OnMount } from '@monaco-editor/react';
+import { Box , Play } from "lucide-react"
 
 interface CustomEditorProps {
 	getValue: () => string;
@@ -23,21 +24,24 @@ export default function page() {
 
 	return (
 		<div className="flex justify-center items-center mt-24 px-4 flex-wrap">
-			<div className="max-w-5xl p-4 bg-card w-full border border-border rounded-lg hover:border-primary mb-4">
-				<div className="rounded-lg  overflow-hidden">
-					<Editor
-						className="h-[500px]"
-						defaultLanguage="sql"
-						defaultValue="// some comment"
-						theme="vs-dark"
-						options={{
-							fontSize: 16,
-						}}
-						onMount={handleEditorDidMount}
-					/>
+			<div className="max-w-5xl p-4 w-full">
+				<h1 className="text-3xl font-bold mb-4 flex gap-2 items-center"><Box size={"1.5em"} />SQL Query Playground</h1>
+				<div className="bg-card w-full border border-border rounded-lg hover:border-primary mb-4 p-4">
+					<div className="overflow-hidden rounded-lg h-[500px]">
+						<Editor
+							className="h-[500px]"
+							defaultLanguage="sql"
+							defaultValue="// some comment"
+							theme="vs-dark"
+							options={{
+								fontSize: 16,
+							}}
+							onMount={handleEditorDidMount}
+						/>
+					</div>
 				</div>
 				<div>
-					<button onClick={getIDEValue} className="btn">Get Value</button>
+					<button onClick={getIDEValue} className="flex items-center gap-2 bg-primary py-2 px-4 rounded-md font-bold text-lg hover:scale-95 transition"><Play /> Query</button>
 				</div>
 			</div>
 		</div>

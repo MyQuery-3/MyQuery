68c97ef735f8878b9493a122fd88deca8c1313a0
suwizx <suwijak.pak@gmail.com>
2024-10-15 12:28:21 +0700

    fuking typescript

--- website/client/app/playground/page.tsx
+++ website/client/app/playground/page.tsx
@@ -1,9 +1,44 @@
 "use client";
 
+import { useRef } from "react"
+import Editor ,{ OnMount } from '@monaco-editor/react';
+
+interface CustomEditorProps {
+	getValue: () => string;
+}
+
 export default function page() {
+
+	const editorRef = useRef<CustomEditorProps>();
+
+	const handleEditorDidMount :OnMount = (editor) => {
+		editorRef.current = editor;
+	}
+
+	function getIDEValue() {
+		if (editorRef.current) {
+			return editorRef.current.getValue();
+		}
+	}
+
 	return (
-		<div className="flex justify-center items-center mt-24">
-			<div className="max-w-5xl p-4 bg-card w-full border border-border">
+		<div className="flex justify-center items-center mt-24 px-4 flex-wrap">
+			<div className="max-w-5xl p-4 bg-card w-full border border-border rounded-lg hover:border-primary mb-4">
+				<div className="rounded-lg  overflow-hidden">
+					<Editor
+						className="h-[500px]"
+						defaultLanguage="sql"
+						defaultValue="// some comment"
+						theme="vs-dark"
+						options={{
+							fontSize: 16,
+						}}
+						onMount={handleEditorDidMount}
+					/>
+				</div>
+				<div>
+					<button onClick={getIDEValue} className="btn">Get Value</button>
+				</div>
 			</div>
 		</div>
 	)

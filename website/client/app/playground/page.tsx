27252d7dd35ccb18b2cf1d4d79b120bf0325c1fa
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-10-30 13:04:16 +0700

    Add DataTable component and enhance SQL Query Playground functionality

--- website/client/app/playground/page.tsx
+++ website/client/app/playground/page.tsx
@@ -5,77 +5,110 @@ import { themeContext } from "../provider/themeProvider";
 import Editor, { OnMount } from "@monaco-editor/react";
 import { Box, Play } from "lucide-react";
 import axios from "axios";
+import DataTable, { TableColumn } from "react-data-table-component";
 
 interface CustomEditorProps {
-	getValue: () => string;
+  getValue: () => string;
+}
+
+interface DataRow {
+  id: number;
+  name: string;
+  email: string;
 }
 
 export default function page() {
-	
-	const { isDark } = useContext(themeContext);
+  const { isDark } = useContext(themeContext);
 
-	useEffect(() => {
-		if (isDark) {
-			setVsTheme("vs-dark");
-		} else {
-			setVsTheme("vs-light");
-		}
-	}, [isDark]);
+  useEffect(() => {
+    if (isDark) {
+      setVsTheme("vs-dark");
+    } else {
+      setVsTheme("vs-light");
+    }
+  }, [isDark]);
 
-	const [vsTheme, setVsTheme] = useState("vs-dark");
-	const [result, setResult] = useState<any>([]);
-	const editorRef = useRef<CustomEditorProps>();
-	const handleEditorDidMount: OnMount = (editor) => {
-		editorRef.current = editor;
-	};
-	function getIDEValue() {
-		if (editorRef.current) {
-			return editorRef.current.getValue();
-		}
-	}
-	// Handle sql query
-	const handleQuery = async () => {
-		try {
-			const query = getIDEValue();
-			const response = await axios.post("http://localhost:8787/api/execute", {
-				query,
-			});
-			setResult(response.data.result);
-			console.log(result);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	return (
-		<div className="flex justify-center items-center mt-24 px-4 flex-wrap">
-			<div className="max-w-5xl p-4 w-full">
-				<h1 className="text-3xl font-bold mb-4 flex gap-2 items-center">
-					<Box size={"1.5em"} />
-					SQL Query Playground
-				</h1>
-				<div className="bg-card w-full border border-border rounded-lg hover:border-primary mb-4 p-4">
-					<div className="overflow-hidden rounded-lg h-[500px]">
-						<Editor
-							className="h-[500px]"
-							defaultLanguage="sql"
-							defaultValue="SELECT * FROM users"
-							theme={vsTheme}
-							options={{
-								fontSize: 18,
-							}}
-							onMount={handleEditorDidMount}
-						/>
-					</div>
-				</div>
-				<div>
-					<button
-						onClick={handleQuery}
-						className="flex items-center gap-2 bg-primary py-2 px-4 rounded-md font-bold text-lg hover:scale-95 transition"
-					>
-						<Play /> Query
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  const [vsTheme, setVsTheme] = useState("vs-dark");
+  const [result, setResult] = useState<any>([]);
+  const editorRef = useRef<CustomEditorProps>();
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+  function getIDEValue() {
+    if (editorRef.current) {
+      return editorRef.current.getValue();
+    }
+  }
+  // Handle sql query
+  const handleQuery = async () => {
+    try {
+      const query = getIDEValue();
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/execute`,
+        {
+          query,
+        }
+      );
+      setResult(response.data.result);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const columns: TableColumn<DataRow>[] = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+    },
+  ];
+
+  return (
+    <div className="flex justify-center items-center mt-24 px-4 flex-wrap">
+      <div className="max-w-5xl p-4 w-full">
+        <h1 className="text-3xl font-bold mb-4 flex gap-2 items-center">
+          <Box size={"1.5em"} />
+          SQL Query Playground
+        </h1>
+        <div className="bg-card w-full border border-border rounded-lg hover:border-primary mb-4 p-4">
+          <div className="overflow-hidden rounded-lg h-[500px]">
+            <Editor
+              className="h-[500px]"
+              defaultLanguage="sql"
+              defaultValue="SELECT * FROM users"
+              theme={vsTheme}
+              options={{
+                fontSize: 18,
+              }}
+              onMount={handleEditorDidMount}
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={handleQuery}
+            className="flex items-center gap-2 bg-primary py-2 px-4 rounded-md font-bold text-lg hover:scale-95 transition"
+          >
+            <Play /> Query
+          </button>
+        </div>
+        <div className="mt-4">
+          <DataTable
+            columns={columns}
+            data={result}
+            pagination
+            highlightOnHover
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
